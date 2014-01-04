fe4b3c4c7687f54901efeefa9fcac4ba479706ac
Szymon Wygnanski <s@finalclass.net>
2014-01-04 21:09:54 +0100

    -

--- server/app.ts
+++ server/app.ts
@@ -20,15 +20,61 @@ app.use(express.static(__dirname + '/../client'));
 app.use(app.router);
 app.use(function (err:any, req:express.Request, res:express.Response, next:Function) {
   console.error(err.stack);
-  res.send(500, 'Something broke!');
+  res.send(500, '500 Internal server error');
 });
 
 // ---------------------------
 // Socket.IO
 // ---------------------------
-io.sockets.on('connection', function (socket) {
-  socket.on('message', function (msg) {
-    io.sockets.emit('message', msg);
-  });
-});
 
+interface ICoordinates {
+  latitude:number;
+  longitude:number;
+}
+
+class User implements ICoordinates {
+  public latitude:number = 0;
+  public longitude:number = 0;
+
+  public distanceTo(user:User) {
+    return getDistanceFromLatLonInKm(
+      this.latitude, this.longitude, 
+      user.latitude, user.longitude);
+  }
+}
+
+function deg2rad(deg:number) : number {
+  return deg * (Math.PI/180)
+}
+
+function getDistanceFromLatLonInKm (lat1:number,lon1:number,lat2:number,lon2:number) : number {
+  var R:number = 6371; // Radius of the earth in km
+  var dLat:number = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon:number = deg2rad(lon2-lon1); 
+  var a:number = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c:number = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d:number = R * c; // Distance in km
+  return d;
+}
+
+io.sockets.on('connection', function (socket) {
+  var user:User = new User();
+  
+  (<any>socket).user = user;
+  socket.on('coords', function (coords:ICoordinates) {2
+    user.latitude = coords.latitude;
+    user.longitude = coords.longitude;
+  });
+  socket.on('message', function (msg) {
+    (<any>io.sockets).clients().forEach(function (client:Socket) {
+      var u:User = (<any>client).user;
+      if (user.distanceTo(u) < 5) {
+        client.emit('message', msg);
+      }
+    });
+  });
+
+});
--- tmp/tsreq/app.js
+++ tmp/tsreq/app.js
@@ -17,15 +17,55 @@ app.use(express.static(__dirname + '/../client'));
 app.use(app.router);
 app.use(function (err, req, res, next) {
     console.error(err.stack);
-    res.send(500, 'Something broke!');
+    res.send(500, '500 Internal server error');
 });
 
-// ---------------------------
-// Socket.IO
-// ---------------------------
+
+var User = (function () {
+    function User() {
+        this.latitude = 0;
+        this.longitude = 0;
+    }
+    User.prototype.distanceTo = function (user) {
+        return getDistanceFromLatLonInKm(this.latitude, this.longitude, user.latitude, user.longitude);
+    };
+    return User;
+})();
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180);
+}
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371;
+    var dLat = deg2rad(lat2 - lat1);
+    var dLon = deg2rad(lon2 - lon1);
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d;
+}
+
 io.sockets.on('connection', function (socket) {
+    var user = new User();
+
+    socket.user = user;
+
+    socket.on('coords', function (coords) {
+        2;
+        user.latitude = coords.latitude;
+        user.longitude = coords.longitude;
+        console.log('setting coords', coords);
+    });
     socket.on('message', function (msg) {
-        io.sockets.emit('message', msg);
+        io.sockets.clients().forEach(function (client) {
+            var u = client.user;
+            console.log('user', u);
+            console.log('Distance:', user.distanceTo(u));
+            if (user.distanceTo(u) < 5) {
+                client.emit('message', msg);
+            }
+        });
     });
 });
 module.exports = app;
